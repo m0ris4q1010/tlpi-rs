5f56243b862fd3955ce6a953d354aa7dba85f840
Yuichi Morisaki <m0ris4q1@gmail.com>
2025-12-26 08:44:00 +0900

    Complete Chapter 12

---
+++ sysinfo/src/bin/procfs_pidmax.rs
@@ -0,0 +1,64 @@
+use std::{os::fd::AsFd, process::Command};
+
+use clap::Parser;
+use nix::{
+    fcntl::{OFlag, open},
+    sys::stat::Mode,
+    unistd::{Whence, lseek, read, write},
+};
+
+use lib::{err_exit, exit_success, fatal};
+
+const MAX_LINE: usize = 100;
+
+#[derive(Parser)]
+struct Cli {
+    new_val: Option<String>,
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    let mut line = [0; MAX_LINE];
+
+    let open_flags = if cli.new_val.is_some() {
+        OFlag::O_RDWR
+    } else {
+        OFlag::O_RDONLY
+    };
+    let file_perms = Mode::empty();
+    let fd = open("/proc/sys/kernel/pid_max", open_flags, file_perms).unwrap_or_else(|e| {
+        err_exit(e, "open".into());
+    });
+
+    let n = read(fd.as_fd(), &mut line).unwrap_or_else(|e| {
+        err_exit(e, "read".into());
+    });
+    let old_val = str::from_utf8(&line[..n]).expect("valid utf-8");
+
+    if cli.new_val.is_some() {
+        print!("Old value: ");
+    }
+    println!("{}", old_val.trim());
+
+    if let Some(val) = cli.new_val {
+        if let Err(e) = lseek(fd.as_fd(), 0, Whence::SeekSet) {
+            err_exit(e, "lseek".into());
+        }
+        let n = write(fd.as_fd(), val.as_bytes()).unwrap_or_else(|e| {
+            err_exit(e, "write".into());
+        });
+        if n != val.len() {
+            fatal("write() failed".into());
+        }
+
+        let output = Command::new("cat")
+            .arg("/proc/sys/kernel/pid_max")
+            .output()
+            .expect("failed to read pid_max");
+        let pid_max = String::from_utf8(output.stdout).expect("");
+        println!("/proc/sys/kernel/pid_max now contains {}", pid_max.trim());
+    }
+
+    exit_success();
+}
---
+++ sysinfo/src/bin/t_uname.rs
@@ -0,0 +1,23 @@
+use nix::sys::utsname::uname;
+
+use lib::{err_exit, exit_success};
+
+fn main() {
+    let uts_name = uname().unwrap_or_else(|e| {
+        err_exit(e, "uname".into());
+    });
+
+    let nodename = uts_name.nodename().to_str().expect("nodename");
+    let sysname = uts_name.sysname().to_str().expect("sysname");
+    let release = uts_name.release().to_str().expect("release");
+    let version = uts_name.version().to_str().expect("version");
+    let machine = uts_name.machine().to_str().expect("machine");
+
+    println!("Node name:   {}", nodename);
+    println!("System name: {}", sysname);
+    println!("Release:     {}", release);
+    println!("Version:     {}", version);
+    println!("Machine:     {}", machine);
+
+    exit_success();
+}
