510f69f92c085107d8dc2f68bd7109f2a54dd07e
Yuichi Morisaki <m0ris4q1@gmail.com>
2025-12-25 11:23:20 +0900

    Complete Chapter 8

---
+++ users_groups/src/lib.rs
@@ -0,0 +1,6 @@
+mod ugid_functions;
+
+pub use ugid_functions::group_id_from_name;
+pub use ugid_functions::group_name_from_id;
+pub use ugid_functions::user_id_from_name;
+pub use ugid_functions::user_name_from_id;
---
+++ users_groups/src/ugid_functions.rs
@@ -0,0 +1,44 @@
+use nix::{
+    libc::{gid_t, uid_t},
+    unistd::{Gid, Group, Uid, User},
+};
+
+use lib::err_exit;
+
+pub fn user_name_from_id(uid: Uid) -> Option<String> {
+    User::from_uid(uid)
+        .unwrap_or_else(|e| {
+            err_exit(e, "getpwuid_r".into());
+        })
+        .map(|user| user.name)
+}
+
+pub fn user_id_from_name(name: &str) -> Option<Uid> {
+    if let Ok(uid) = uid_t::from_str_radix(name, 10) {
+        return Some(Uid::from_raw(uid));
+    }
+    User::from_name(name)
+        .unwrap_or_else(|e| {
+            err_exit(e, "getpwnam_r".into());
+        })
+        .map(|user| user.uid)
+}
+
+pub fn group_name_from_id(gid: Gid) -> Option<String> {
+    Group::from_gid(gid)
+        .unwrap_or_else(|e| {
+            err_exit(e, "getgrgid_r".into());
+        })
+        .map(|group| group.name)
+}
+
+pub fn group_id_from_name(name: &str) -> Option<Gid> {
+    if let Ok(gid) = gid_t::from_str_radix(name, 10) {
+        return Some(Gid::from_raw(gid));
+    }
+    Group::from_name(name)
+        .unwrap_or_else(|e| {
+            err_exit(e, "getgrnam_r".into());
+        })
+        .map(|group| group.gid)
+}
